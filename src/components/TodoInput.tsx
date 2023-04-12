945c3e86b2357f7e2f804f7c5b8eabb03e8e230a
AlanJhonatan <alan.amorim.franca@gmail.com>
2023-04-12 03:51:49 +0200

    feat:add button required element and style

--- src/components/TodoInput.tsx
+++ src/components/TodoInput.tsx
@@ -1,10 +1,11 @@
+import { PlusCircle } from 'phosphor-react'
 import styles from './TodoInput.module.css'
 
 export function TodoInput() {
   return (
     <form className={styles.todoForm}>
       <input className={styles.todoInput} placeholder='Adicione uma nova tarefa' type="text" />
-      <button className={styles.buttonCreate} type="submit">Criar +</button>
+      <button className={styles.buttonCreate} type="submit">Criar <PlusCircle weight='bold' size={16} /></button>
     </form>
   )
 }

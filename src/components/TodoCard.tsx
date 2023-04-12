fbb94ef416c21fabdcaab3b5bb0fa4bb9abbff83
AlanJhonatan <alan.amorim.franca@gmail.com>
2023-04-12 03:35:13 +0200

    feat: add Todo Cards styles

---
+++ src/components/TodoCard.tsx
@@ -0,0 +1,49 @@
+import { CheckCircle, Circle, Icon, Trash } from 'phosphor-react'
+import styles from './TodoCard.module.css'
+import { MouseEvent, MouseEventHandler, useState } from 'react'
+
+
+export interface Todo {
+  isFinished: boolean
+  text: string
+}
+
+interface TodoProps {
+  toDo: Todo
+}
+
+export function TodoCard({ toDo }: TodoProps) {
+  const [mouseHover, setMouseHover] = useState(false)
+
+  return (
+    <div className={styles.container}>
+      {
+        toDo.isFinished ?
+          <>
+            <span
+              className={styles.checkedCircle}
+              onMouseOver={() => setMouseHover(true)}
+              onMouseOut={() => setMouseHover(false)}
+            >
+              <CheckCircle size={24} weight='fill' />
+            </span>
+            <p className={styles.textChecked}>{toDo.text}</p>
+          </>
+        : 
+          <>
+            <Circle 
+              size={24} 
+              weight={mouseHover ? 'duotone' : 'bold'}
+              onMouseOver={() => setMouseHover(true)}
+              onMouseOut={() => setMouseHover(false)}
+              className={styles.circle}
+            />
+            <p>{toDo.text}</p>
+          </>
+      }
+
+      
+      <Trash size={20} className={styles.buttonDelete} />
+    </div>
+  )
+}
